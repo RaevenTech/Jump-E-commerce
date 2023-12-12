d3a031f8a591233cb93146b86b29e15a41c352d1
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-12-12 17:12:44 +0000

    product array added to js

--- script.js
+++ script.js
@@ -2,9 +2,90 @@ const sliderWrapper = document.getElementById("slider_wrapper")
 const menuItem = document.querySelectorAll(".menu_item")
 //console.log(menuItem)
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./image/air-force-blk.png",
+      },
+      {
+        code: "darkblue",
+        img: "./image/air-force-blk.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./image/airjordan.png",
+      },
+      {
+        code: "green",
+        img: "./image/airjordan.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./image/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./image/blazer.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./image/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./image/crater.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./image/hippie2.png",
+      },
+      {
+        code: "green",
+        img: "./image/hippie.png",
+      },
+    ],
+  },
+];
+
+
 menuItem.forEach((item, i) => {
   item.addEventListener("click", () => {
     sliderWrapper.style.transform = `translateX(${-100 * i}vw)`
   })
 })
+
+
 
